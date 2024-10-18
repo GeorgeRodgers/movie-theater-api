5b7eed9ac54a7271856f589c70d84a0124073daf
George Rodgers <george.rodgers.chem@gmail.com>
2024-10-18 15:04:52 +0100

    Notes added for src/app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
+// Imports express and creates express server
 const express = require("express");
 const app = express();
+
+// Imports routers
 const { userRouter } = require(`../routes/Users`);
 const { showRouter } = require(`../routes/Shows`);
 
+// Middleware for server
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(`/users`, userRouter);

ddb4d367ac38a29de8357540983273e15e230547
George Rodgers <george.rodgers.chem@gmail.com>
2024-10-18 15:02:34 +0100

    Notes added for routes/Users.js

--- routes/Users.js
+++ routes/Users.js
@@ -4,21 +4,25 @@ const { check, validationResult } = require(`express-validator`);
 
 const userRouter = Router();
 
+// GET route to get all users
 userRouter.get(`/`, async (req, res) => {
     const users = await User.findAll({});
     res.json(users);
 });
 
+// GET route to get a user by id
 userRouter.get(`/:id`, async (req, res) => {
     const user = await User.findByPk(req.params.id);
     res.json(user);
 });
 
+// GET route to get a user by id with shows watched
 userRouter.get(`/:id/shows`, async (req, res) => {
     const userWithShows = await User.findByPk(req.params.id, {include: {model: Show}});
     res.json(userWithShows);
 });
 
+// PUT route to add a show to a user
 userRouter.put(`/:userid/shows/:showid`, async (req, res) => {
     const user = await User.findByPk(req.params.userid); 
     const show = await Show.findByPk(req.params.showid);

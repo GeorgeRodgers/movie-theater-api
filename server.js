073b2860210f0d94e4e50148bdfb353b680d38a6
George Rodgers <george.rodgers.chem@gmail.com>
2024-10-18 15:06:31 +0100

    Notes added for server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+// Import application server and database
 const {app} = require(`./src/app`)
 const {db} = require("./db/connection");
-const port = 3000;
+const port = 3000; // assigns a port for the server
 
+// Initializes the server
 app.listen(port, () => {
     db.sync();
     console.log(`Listening at http://localhost:${port}/`);
